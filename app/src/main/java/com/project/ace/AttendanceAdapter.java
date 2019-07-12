ff2c1691d09ff7a96fb46e85d55990c0c29056e4
Dhruv Vohra <dhruv.v405@gmail.com>
2019-07-12 14:36:51 +0530

    Attendance manager done

--- app/src/main/java/com/project/ace/AttendanceAdapter.java
+++ app/src/main/java/com/project/ace/AttendanceAdapter.java
@@ -2,6 +2,8 @@ package com.project.ace;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Handler;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -12,34 +14,25 @@ import android.widget.Toast;
 
 import com.firebase.ui.firestore.FirestoreRecyclerAdapter;
 import com.firebase.ui.firestore.FirestoreRecyclerOptions;
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.DocumentReference;
-import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FieldValue;
 import com.google.firebase.firestore.FirebaseFirestore;
 
+
 public class AttendanceAdapter extends FirestoreRecyclerAdapter<Attendance, AttendanceAdapter.AttendanceHolder> {
 
-    FirebaseAuth mAuth = FirebaseAuth.getInstance();
-    FirebaseUser user = mAuth.getCurrentUser();
-    private FirebaseFirestore db = FirebaseFirestore.getInstance();
-
-    int updateClassAttended = 0, updateClassTotal = 0;
-
+    private FirebaseFirestore db = FirebaseFirestore.getInstance();     //firestore instance
 
     public AttendanceAdapter(@NonNull FirestoreRecyclerOptions<Attendance> options) {
         super(options);
-        Log.d("Hi", String.valueOf(getItemCount()));
+        Log.d("count", String.valueOf(getItemCount()));
     }
 
     @Override
     protected void onBindViewHolder(@NonNull final AttendanceHolder holder, int position, @NonNull Attendance model) {
         int classTotal, classAttended;
         float attendancePercentage, target, classRequired;
-        String attendanceFraction, attendancePerc, finalclassRequired;
+        final String attendanceFraction, attendancePerc, finalclassRequired;
 
         classTotal = model.getClassTotal();
         classAttended = model.getClassAttended();
@@ -55,13 +48,13 @@ public class AttendanceAdapter extends FirestoreRecyclerAdapter<Attendance, Atte
             attendancePercentage = ((float) classAttended / (float) classTotal) * 100;   //percentage - float
             attendancePerc = (String.format("%.2f", attendancePercentage) + "%");     //percentage - string
 
-            attendanceFraction = (Integer.toString(classAttended) + "/" + Integer.toString(classTotal));        //fraction - string
+            attendanceFraction = ((classAttended) + "/" + (classTotal));        //fraction - string
 
             target = model.getTarget();
 
 
             if (target > attendancePercentage) {
-                classRequired = (float) ((target * classTotal) - (100 * classAttended)) / (float) (100 - target);
+                classRequired = ((target * classTotal) - (100 * classAttended)) / (100 - target);
                 finalclassRequired = Integer.toString((int) Math.ceil((double) classRequired));
                 holder.textViewMessage.setText("Attend next " + finalclassRequired + " classes to reach target");
             } else {
@@ -72,6 +65,7 @@ public class AttendanceAdapter extends FirestoreRecyclerAdapter<Attendance, Atte
             holder.textViewCourseCode.setText(model.getCourseCode());
             holder.textViewFraction.setText(attendanceFraction);
             holder.textViewPercentage.setText(attendancePerc);
+
         }
 
         holder.attendedButton.setOnClickListener(new View.OnClickListener() {
@@ -103,8 +97,7 @@ public class AttendanceAdapter extends FirestoreRecyclerAdapter<Attendance, Atte
         String id = getSnapshots().getSnapshot(position).getId();
         final DocumentReference docRef = db.collection("Attendance").document(id);
 
-        docRef.update("classAttended", FieldValue.increment(1));
-        docRef.update("classTotal", FieldValue.increment(1));
+        docRef.update("classAttended", FieldValue.increment(1) , "classTotal",FieldValue.increment(1));
     }
 
     public void missedClass(int position) {
